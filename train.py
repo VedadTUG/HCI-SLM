66b777d5e32554885ab11192a1c3c9e58347b4cc
dogpi-git <rieder1009@gmail.com>
2024-06-05 14:40:38 +0200

    added new generate lyrics function such that it is the same as for nanogpt now only input needs to be made

--- train.py
+++ train.py
@@ -12,7 +12,9 @@ import glob
 from codecarbon import OfflineEmissionsTracker
 
 import logging
+
 tracker = OfflineEmissionsTracker(country_iso_code='AUT')
+
 
 def find_csv_files(path, file_extension="*.csv"):
     try:
@@ -23,6 +25,7 @@ def find_csv_files(path, file_extension="*.csv"):
     except Exception as e:
         logging.error(f"Error finding files in {path}: {e}")
         return []
+
 
 def read_csv_files(file_paths, column_name='Lyrics'):
     df_list = []
@@ -37,6 +40,7 @@ def read_csv_files(file_paths, column_name='Lyrics'):
             logging.error(f"Error reading {file}: {e}")
     return pd.concat(df_list, ignore_index=True) if df_list else pd.DataFrame()
 
+
 def concatenate_lyrics(df, column_name='Lyrics'):
     if column_name in df.columns:
         try:
@@ -47,6 +51,7 @@ def concatenate_lyrics(df, column_name='Lyrics'):
     else:
         logging.warning(f"Column {column_name} not found in DataFrame")
         return ""
+
 
 def load_and_concatenate_lyrics(path, file_extension="*.csv", column_name='Lyrics'):
     files = find_csv_files(path, file_extension)
@@ -66,30 +71,36 @@ print(' '.join(sorted(set(lyrics))))
 
 import re
 
+
 def replace_characters(text, replacement_dict):
     return text.translate(str.maketrans(replacement_dict))
+
 
 def remove_patterns(text, pattern_list):
     for pattern in pattern_list:
         text = re.sub(pattern, '', text)
     return text
 
+
 def clean_lyrics(lyrics):
     replace_with_space = ['\u2005', '\u200b', '\u205f', '\xa0', '-']
-    replace_letters = {'í':'i', 'é':'e', 'ï':'i', 'ó':'o', ';':',', '‘':'\'', '’':'\'', ':':',', 'е':'e'}
-    remove_list = ['\)', '\(', '–','"','”', '"', '\[.*\]', '.*\|.*', '—']
+    replace_letters = {'í': 'i', 'é': 'e', 'ï': 'i', 'ó': 'o', ';': ',', '‘': '\'', '’': '\'', ':': ',', 'е': 'e'}
+    remove_list = ['\)', '\(', '–', '"', '”', '"', '\[.*\]', '.*\|.*', '—']
     lyrics = replace_characters(lyrics, replace_letters)
     for string in replace_with_space:
         lyrics = lyrics.replace(string, ' ')
     lyrics = remove_patterns(lyrics, remove_list)
     return lyrics
+
+
 cleaned_lyrics = clean_lyrics(lyrics)
-#%%
+# %%
 print(''.join(sorted(set(cleaned_lyrics))))
-#%%
+# %%
 cleaned_lyrics
-#%%
+# %%
 from datasets import Dataset
+
 
 def create_train_test_datasets(cleaned_lyrics, train_ratio=0.95, segment_length=500):
     split_point = int(len(cleaned_lyrics) * train_ratio)
@@ -99,10 +110,12 @@ def create_train_test_datasets(cleaned_lyrics, train_ratio=0.95, segment_length=
                            for i in range(0, len(train_data), segment_length)]
     train_dataset = Dataset.from_dict({'text': train_data_segments})
     return train_dataset, test_data
+
+
 train_dataset, test_data = create_train_test_datasets(cleaned_lyrics)
-#%%
+# %%
 print(len(train_dataset))
-#%%
+# %%
 train_dataset
 
 # Check the dataset structure
@@ -115,7 +128,9 @@ Dataset({
 """
 
 from pprint import pprint
+
 pprint(train_dataset[0])
+
 
 def load_quantized_model(model_identifier: str, compute_dtype: torch.dtype) -> AutoModelForCausalLM:
     quantization_config = BitsAndBytesConfig(
@@ -124,13 +139,14 @@ def load_quantized_model(model_identifier: str, compute_dtype: torch.dtype) -> A
         bnb_4bit_use_double_quant=True,
         bnb_4bit_compute_dtype=compute_dtype,
     )
-    model =  AutoModelForCausalLM.from_pretrained(
+    model = AutoModelForCausalLM.from_pretrained(
         model_identifier,
         quantization_config=quantization_config,
         device_map="auto",
         trust_remote_code=True,
     )
     return model
+
 
 model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 model = load_quantized_model(model_name, torch.bfloat16)
@@ -141,17 +157,21 @@ tokenizer.pad_token = tokenizer.eos_token
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+
 def generate_lyrics(query, model):
     encoding = tokenizer(query, return_tensors="pt").to(device)
-    generation_config = GenerationConfig(max_new_tokens=250, pad_token_id = tokenizer.eos_token_id,repetition_penalty=1.3, eos_token_id = tokenizer.eos_token_id)
+    generation_config = GenerationConfig(max_new_tokens=500, pad_token_id=tokenizer.eos_token_id,
+                                         repetition_penalty=1.3, eos_token_id=tokenizer.eos_token_id)
+    num_samples = 10
+    for k in range(num_samples):
+        outputs = model.generate(input_ids=encoding.input_ids, generation_config=generation_config)
+        text_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        print('INPUT\n', query, '\n\nOUTPUT\n', text_output[len(query):])
+        print("---------------------")
 
-    outputs = model.generate(input_ids=encoding.input_ids, generation_config=generation_config)
 
-    text_output = tokenizer.decode(outputs[0],skip_special_tokens=True)
-    print('INPUT\n', query, '\n\nOUTPUT\n', text_output[len(query):])
 
-generate_lyrics(test_data[200:700], model)
-#%%
+# %%
 model = prepare_model_for_kbit_training(model)
 lora_alpha = 32
 lora_dropout = 0.05
@@ -168,7 +188,7 @@ output_dir = "hf-username/tinylama_taylor_swift"
 per_device_train_batch_size = 3
 gradient_accumulation_steps = 2
 optim = "paged_adamw_32bit"
-save_strategy="steps"
+save_strategy = "steps"
 save_steps = 10
 logging_steps = 10
 learning_rate = 2e-3
@@ -207,3 +227,6 @@ tracker.start()
 trainer.train()
 tracker.stop()
 
+#Here load input set for generating task
+#Also track here
+#generate_lyrics(test_data[200:700], model)
